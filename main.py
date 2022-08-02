a64adb6f596b707a660d146f93c1fa258374ad6e
Viagounet <ismael.rousseau@gmail.com>
2022-08-02 14:43:26 +0200

    14h43

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import dash
 import dash_bootstrap_components as dbc
 from dash import Output, Input, State, html, ALL, MATCH
+from dash_iconify import DashIconify
+from flask import Flask
 
 from phone import PhoneColorPalette, Phone
 
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = Flask(__name__)
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], server=server)
+
 
 phones = [Phone("blue", height="65vh", number="+1 719 212 2797", id=0),
           Phone("green", height="65vh", id=1),
@@ -13,10 +17,20 @@ phones = [Phone("blue", height="65vh", number="+1 719 212 2797", id=0),
 
 app.layout = dbc.Container(
     [html.H1("Your virtual phones : "), html.Div([html.Div([phone, html.P(phone.number)],
-                                                           className="d-flex flex-column justify-content-center align-items-center") for phone in phones],
-             className="d-flex flex-row")],
+                                                           className="d-flex flex-column justify-content-center align-items-center")
+                                                  for phone in phones],
+                                                 className="d-flex flex-row"),
+     ],
     className="p-5 d-flex flex-column")
-#[phone, html.P(phone.number)]
+
+
+# [phone, html.P(phone.number)]
+def get_phone_by_number(number):
+    for phone in phones:
+        if phone.number == number:
+            return phone
+    return None
+
 
 @app.callback(
     Output({'label': 'phone-text', 'index': MATCH}, "children"),
@@ -25,12 +39,69 @@ app.layout = dbc.Container(
     prevent_initial_call=True
 )
 def send_input(n_clicks, value):
+    phone = get_phone_by_number("+1 719 212 2797")
     if n_clicks is None:
         return "Please enter a number"
     else:
-        phones[0].send_sms(value, "+33637890663")
-        return "The number you entered is {}".format(value)
+        phone.messages_list.append(value)
+        children = [html.Div(
+            f"{message}",
+            style={"color": "black", "background-color": "white", "border-radius": "10px", "padding": "5px",
+                   "border": "1px solid gray", "font-size": "0.8rem", "margin": "0.5rem"}) for message in
+            phone.messages_list]
+
+        print("Sending message: " + value + " to +33637890663 from " + phone.number)
+        phone.send_sms(value, "+33637890663")
+        return children
+
+
+palette = PhoneColorPalette("blue")
+
+
+@app.callback(
+    Output({'label': 'phone-container', 'index': MATCH}, "children"),
+    Input({'label': 'phone-go-contact', 'index': MATCH}, "n_clicks"),
+    State({'label': 'phone-container', 'index': MATCH}, "children"),
+    prevent_initial_call=True
+)
+def go_contact(n_clicks, children):
+    if n_clicks is None:
+        return children
+    else:
+        id = 5000
+        header = html.Div(
+            [
+                html.Div(
+                    [html.Div(
+                        DashIconify(icon="openmoji:anger-symbol", width=30, color=f"{palette.high_contrast}")),
+                        html.H6("Absolute 0", style={"color": "black"})],
+                    className="d-flex flex-row justify-content-center align-items-center p-2 gap-2"),
+            ],
+
+            id={'label': 'phone-top', 'index': id},
+            style={"width": "100%",
+                   "min-height": "10%",
+                   "position": "relative",
+                   "height": "10%", "background-color": f"{palette.top}",
+                   "borderRadius": "10px 10px 0px 0px",
+                   "border-bottom": f"3px ridge {palette.buttons}"},
+            className="d-flex flex-row justify-content-center align-items-center p-2")
+
+        children = [html.Div([  # Equivalent to `html.Div([...])`
+            # Header
+            header,
+
+            # Messages
+            html.Div(),
+
+            # Footer
+        ], style={"position": "absolute", "top": "0px", "left": "0px", "bottom": "0px", "right": "0px",
+                  "overflow": "auto"}),
+            html.Div(),
+
+        ]
+        return children
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server()
--- phone.py
+++ phone.py
@@ -10,38 +10,38 @@ from send_sms import send_sms_to
 class PhoneColorPalette:
     def __init__(self, color):
         self.colors = {
-            'blue': ["#BBDEFB", "#EEFFFF", "#8AACC8"],
-            'green': ["#C8E6C9", "#FBFFFC", "#97B498"],
-            'pink': ["#F8BBD0", "#FFFFFF", "#C9B2BA"],
-            'white': ["#FAFAFA", "#FFFFFF", "#C7C7C7"], }
+            'blue': ["#BBDEFB", "#EEFFFF", "#8AACC8", "#293137"],
+            'green': ["#C8E6C9", "#FBFFFC", "#97B498", "#353D35"],
+            'pink': ["#F8BBD0", "#FFFFFF", "#C9B2BA", "#423238"],
+            'white': ["#FAFAFA", "#FFFFFF", "#C7C7C7", "#1A1A1A"], }
 
         self.top = self.colors[color][0]
         self.bg = self.colors[color][1]
         self.buttons = self.colors[color][2]
+        self.high_contrast = self.colors[color][3]
 
 
 class Phone(html.Div):
     def __init__(self, color, id, number="None", height="auto", width="auto", **kwargs):
         self.number = number
-        self.recipient = "TDK (+33 6 37 89 06 63)"
-        self.messages = html.Div([html.P(
-            f"Test",
-            style={"color": "black", "background-color": "white", "border-radius": "10px", "padding": "10px",
-                   "border": "1px solid gray"}),
-            html.P(id={'label': 'phone-text', 'index': id}, style={"color": "black"},
-                   )], className="p-2", style={})
-
+        self.recipient = "TDK"
+        self.messages_list = ["Yes this is a test", "and another test"]
         if self.number == "None":
             self.number = "No number set"
             self.recipient = ""
-            self.messages = html.Div()
+            self.messages_list = []
+
+        self.messages = html.Div([html.Div(
+            f"{message}",
+            style={"color": "black", "background-color": "white", "border-radius": "10px", "padding": "5px",
+                   "border": "1px solid gray", "font-size": "0.8rem", "margin": "0.5rem"}) for message in
+            self.messages_list],
+        id={'label': 'phone-text', 'index': id})
 
         if height != "auto" and width == "auto":
             value = float(re.sub("[^0-9]", "", height))
             measure = re.sub('[^a-zA-Z]+', '', height)
-            print(value)
             width = str(value / 2) + measure
-            print(height, width)
         elif height == "auto" and width != "auto":
             value = float(re.sub("[^0-9]", "", width))
             measure = re.sub('[^a-zA-Z]+', '', width)
@@ -51,21 +51,31 @@ class Phone(html.Div):
         self.footer = html.Div([
             html.Div([
                 dbc.Input(placeholder="Your text message", id={'label': 'phone-text-area', 'index': id}),
-                html.Div(DashIconify(icon="material-symbols:send-rounded", width=30, color=f"{palette.buttons}"))
+                html.Div(DashIconify(icon="material-symbols:send-rounded", width=30, color=f"{palette.buttons}",
+                                     ), id={'label': 'send-message-phone', 'index': id}, className="send-button")
             ], className="d-flex flex-row justify-content-center align-items-center p-2 gap-2")
         ],
             style={"width": "100%", "position": "absolute", "bottom": "0%", "background-color": f"{palette.bg}",
                    "borderRadius": "0px 0px 10px 10px"})
 
-        self.header = html.Div([html.H6(self.recipient, style={"color": "black"})],
-                               id={'label': 'phone-top', 'index': id},
-                               style={"width": "100%",
-                                      "min-height": "10%",
-                                      "position": "relative",
-                                      "height": "10%", "background-color": f"{palette.top}",
-                                      "borderRadius": "10px 10px 0px 0px",
-                                      "border-bottom": f"3px ridge {palette.buttons}"},
-                               className="d-flex flex-row justify-content-center align-items-center p-2")
+        self.header = html.Div(
+            [
+                html.Div(
+                    [html.Div(
+                        DashIconify(icon="material-symbols:contact-page", width=30, color=f"{palette.high_contrast}"),
+                        className="send-button", id={'label': 'phone-go-contact', 'index': id}),
+                        html.H6(self.recipient, style={"color": "black"})],
+                    className="d-flex flex-row justify-content-center align-items-center p-2 gap-2"),
+            ],
+
+            id={'label': 'phone-top', 'index': id},
+            style={"width": "100%",
+                   "min-height": "10%",
+                   "position": "relative",
+                   "height": "10%", "background-color": f"{palette.top}",
+                   "borderRadius": "10px 10px 0px 0px",
+                   "border-bottom": f"3px ridge {palette.buttons}"},
+            className="d-flex flex-row justify-content-center align-items-center p-2")
 
         super().__init__(
             [html.Div([  # Equivalent to `html.Div([...])`
@@ -84,7 +94,9 @@ class Phone(html.Div):
 
             style={'color': 'white', 'backgroundColor': f"{palette.bg}",
                    "width": width, "height": height, "border": "7px gray ridge",
-                   "borderRadius": "20px", "margin": "10px", "position": "relative"}
+                   "borderRadius": "20px", "margin": "10px", "position": "relative"},
+            className="phone",
+            id={'label': 'phone-container', 'index': id},
         )
 
     def send_sms(self, content, recipient):
